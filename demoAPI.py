ac9a027d14d17d9128c6c4e59e0414e448ae65cf
tran-kim-quang <107093325+tran-kim-quang@users.noreply.github.com>
2025-04-23 17:46:47 +0700

    Update demoAPI.py

--- demoAPI.py
+++ demoAPI.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers = ["*"],
 )
-# Định nghĩa model cho dữ liệu đầu vào
+# Model
 class ChatInput(BaseModel):
     message: str
 @app.get("/")
@@ -19,15 +19,12 @@ async def root():
 @app.post("/chat")
 async def chat_api(input_data: ChatInput):
     user_input = input_data.message
-    
-    # Nếu nhận được 'exit', trả về thông báo tắt chatbot
+
     if user_input.lower() == 'exit':
         return {"response": "Chatbot đã tắt"}
-    
-    # Gọi các hàm có sẵn để xử lý
+
     intent_tag = predict_intent(user_input)
     response = get_response(intent_tag)
-    # thêm message là success hay fail gì đó, thêm đống try catch xử lý, sau dễ debug hơn, ví dụ nếu k phản hồi từ AI đc thì message là fail, response = ""
     return {"response": response}
 
 # Chạy API với uvicorn
